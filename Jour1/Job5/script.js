7b108381716dd63431d82a13fbb8f1c8e3933f4f
Uthman <wakfabrio@gmail.com>
2025-03-11 14:57:21 +0100

    runtrackJS Jobs Jour1

---
+++ Jour1/Job5/script.js
@@ -0,0 +1,4 @@
+let joursdeSemaines=['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+for (let i=0 ; i<joursdeSemaines.length; i++){
+    console.log(joursdeSmaines[i]);
+}
